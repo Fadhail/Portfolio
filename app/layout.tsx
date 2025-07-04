34d02f49cadc6fe226fd6adc8484c6bba1f59975
Fadhail <mochammadfadhail2@gmail.com>
2025-07-04 12:14:29 +0700

    Change Tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Fadel - AI Engineer & System Developer",
+  title: "Fadel Portfolio",
   authors: [{ name: "Fadel" }],
   icons: {
     icon: "/icon-f.svg",
